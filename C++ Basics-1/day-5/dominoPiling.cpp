8e704d9a70a652c2b88103401201a58aa53d48be
Abhinav <abhinavkashyap061@gmail.com>
2022-05-30 10:20:00 +0530

    daily task day-5 done ✔

--- C++ Basics-1/day-5/dominoPiling.cpp
+++ C++ Basics-1/day-5/dominoPiling.cpp
@@ -14,38 +14,40 @@ Problem link: https://codeforces.com/problemset/problem/50/A
 #define ll long long
 #define int_max 2147483647
 #define int_min -2147483648
+#define endl '\n'
 
 using namespace std;
 
 int main(){
 
+	ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int m,n;
 	cin>>m>>n;
 
-	// board of M x N squares
-	// given -> unlimited no. of 2x1 squares
-	// allowed to rotate the pieces
+	/*
 
-	// each domino covers two squares
-	// no two dominos overlap
-	// inside the borad, can overlap
+	- board of M x N squares
+	- given -> unlimited no. of 2x1 squares
+	- allowed to rotate the pieces
 
-	int count=0;
+	- each domino covers two squares
+	- no two dominos overlap inside the borad, can overlap
 
-	if( m<n ){
+	(2x1)x + (1x2)y<=nm
+ 	2x+2y <=(nm)
+	x+y<=(nm)/2
+	x is 2x1 tiles
+	y is 1 x2 tiles
 
-		count = m*2;
+	The answer will always be (n*m)/2
+	Cause you can always fill up the board with dominos if there are even number of cells
+	Otherwise one cell will remain
 
-		
+	*/
 
-	}else if( m>=n ){
-
-		count = n;
-
-		if( m%count==1 || m==n ){
-			count++;
-		}
-	}
+	int count = (m*n)/2;
 
 		
 	cout<<count<<endl;
---
+++ C++ Basics-2/day-1/theatreSquare.cpp
@@ -0,0 +1,40 @@
+/*
+
+Author: Abhinav Kashyap
+
+Portfolio: https://abhinavkashyap061.github.io
+
+Problem Link: https://codeforces.com/problemset/problem/1/A
+
+*/
+
+#include <bits/stdc++.h>
+
+#define ll long long
+#define int_max 2147483647
+#define int_min -2147483648
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // int test_cases;
+    // cin >> test_cases;
+    // while (test_cases--)
+    // {
+
+    //     // your code goes here
+    
+    // }
+
+    ll n,m,a;
+    cin>>n>>m>>a;
+
+    
+
+    return 0;
+}
