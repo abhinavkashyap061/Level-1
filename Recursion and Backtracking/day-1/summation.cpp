b5f11b4acf855934dbb67e600e5b1b27ce099b96
Abhinav <abhinavkashyap061@gmail.com>
2022-06-14 17:35:16 +0530

    update

--- Recursion and Backtracking/day-1/summation.cpp
+++ Recursion and Backtracking/day-1/summation.cpp
@@ -17,12 +17,47 @@ Problem link: https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/L
 
 using namespace std;
 
+int summation( int a[], int n, int sum){
+
+    if( n==0 ){
+
+        sum+=a[n];
+        return sum;
+
+    }
+
+    sum+=a[n];
+    
+    /*
+        returning the value i got from each recursive call
+        when n==0 it will return the final value of sum, 
+        it will exit from recursive calls stack frame
+        then this value will be returned in every other calls
+    */
+
+    return summation(a, n-1, sum);
+
+
+}
+
 int32_t main(){
 
 	ios::sync_with_stdio(false);
     cin.tie(NULL);
 
 	// your code goes here
+
+    int n;
+    cin>>n;
+
+    int a[n];
+    for( int i=0; i<n; i++){
+        cin>>a[i];
+    }
+
+    int sum=0;
+    
+    cout<<summation( a, n-1, sum)<<endl;
 
     return 0;
 
---
+++ Recursion and Backtracking/day-2/log2.cpp
@@ -0,0 +1,29 @@
+/*
+
+Author: Abhinav Kashyap
+
+Portfolio: https://abhinavkashyap061.github.io
+
+Problem link: https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/P
+
+*/
+
+#include <bits/stdc++.h>
+
+#define int long long int
+#define int_max 2147483647
+#define int_min -2147483648
+#define endl '\n'
+
+using namespace std;
+
+int32_t main(){
+
+	ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	// your code goes here
+
+    return 0;
+
+}
