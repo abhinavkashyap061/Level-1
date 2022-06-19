f685c767327cd2318a3a71b428261df9a2ad5795
Abhinav <abhinavkashyap061@gmail.com>
2022-06-19 12:51:18 +0530

    Update numberOfWays.cpp

--- Recursion and Backtracking/day-2/numberOfWays.cpp
+++ Recursion and Backtracking/day-2/numberOfWays.cpp
@@ -17,12 +17,32 @@ Problem link: https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/Y
 
 using namespace std;
 
+int sol( int s, int e ){
+
+    // base case 1
+    if( s==e ){
+        return 1;
+    }
+    //base case2
+    if( s>e ){
+        return 0;
+    }
+
+    //computation 
+    return sol(s+1, e) + sol(s+2, e) + sol(s+3, e);
+}
+
 int32_t main(){
 
 	ios::sync_with_stdio(false);
     cin.tie(NULL);
 
 	// your code goes here
+
+    int s,e;
+    cin>>s>>e;
+
+    cout<< sol(s,e) <<endl;
 
     return 0;
 
