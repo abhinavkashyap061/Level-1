09fa4f1441446401645a1ebfbc78c4e217d41564
Abhinav <abhinavkashyap061@gmail.com>
2022-07-27 11:31:31 +0530

    update

---
+++ Number Theory-2/day-1/gcdProblem.cpp
@@ -0,0 +1,35 @@
+/*
+
+Author: Abhinav Kashyap
+
+Portfolio: https://abhinavkashyap061.github.io
+
+Problem Link: https://codeforces.com/problemset/problem/1617/B
+
+*/
+
+#include <bits/stdc++.h>
+
+#define ll long long
+#define int_max 2147483647
+#define int_min -2147483648
+#define endl '\n'
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int test_cases;
+    cin >> test_cases;
+    while (test_cases--)
+    {
+
+        // your code goes here
+    
+    }
+
+    return 0;
+}
